cb6b79d86c9856b280f37cd740d5780e3a6b1522
LUIZ MENDES <LZ@kuttner.loc>
2020-11-17 15:55:38 -0300

    link home

--- front/nav.php
+++ front/nav.php
@@ -11,7 +11,7 @@
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="/front/visualizacao_ocs.php">COC</a>
+        <a class="navbar-brand" href="javascript:void(0)">COC</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
